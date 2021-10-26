7f6cb34010bf6eb5ed5283084e683ff70357c2ba
DrewMcD25 <drewmcd25@yahoo.com>
2021-10-26 17:45:09 -0500

    still working on bugs

--- src/components/register.js
+++ src/components/register.js
@@ -1,8 +1,13 @@
+import React, {useState} from 'react'
+import { Link, useHistory } from 'react-router-dom';
 
 
 const Register = () => {
+    let history = useHistory();
+    const [user, setUser] = useState({username:'', password:''});
+    const [password, setPassword] = useState({password:''});
+
     async function handleSubmit(event) {
-        
         event.preventDefault()
         await fetch('https://fitnesstrac-kr.herokuapp.com/api/users/register', {
             method: "POST",
@@ -10,7 +15,6 @@ const Register = () => {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                
                     username: event.target.username.value,
                     password: event.target.password.value
                 
@@ -21,18 +25,35 @@ const Register = () => {
             console.log(result);
         })
         .catch(console.error);
-               { alert(error.message)('Uh Oh Something Went Wrong')
-        }
-
+               
+        
+ 
     }
-
+    async function onSubmit(event) {
+        event.preventDefault()
+        if(user.username.length <1 ){
+            alert('username is required to register')
+        }
+        else if( user.password.length <1 ){
+            alert('password is required to register') 
+        }
+        else if(user.username.length <3){
+            alert('username must be longer than 3 characters')
+        }
+        else if(user.password.length <3){
+            alert('password must be longer than 3 characters')
+        }
+        else{
+            handleSubmit();
+        }
+    }
 
     return (
         <div id="register">
             <h1>REGISTER</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" required name="username" placeholder="username"></input>
-                <input type="password" required name="password" placeholder="password"></input>
+            <form onSubmit={handleSubmit, onSubmit}>
+                <input type="text" required name="username" value={user.username} onChange={onSubmit} placeholder="username"></input>
+                <input type="password" required name="password" value={user.password} onChange={onsubmit} placeholder="password"></input>
                 <button>Register</button>
             </form>
             <h2>Already a member? Head over to the Login page and get signed in!</h2>
